f5b33779ba855da5b86aa82573dbf1ff04035def
IvanHunters <ohotnikovivan@yandex.ru>
2019-11-27 23:58:10 +0200

    test_2

--- install.php
+++ install.php
@@ -1,5 +1,5 @@
 <?php
 $name_bot = readline("Введите название для бота: ");
 echo "\nКакой бот нужен?\n1 - Vk\n2 - Telegram\n3 - Facebook\n";
-$type_bot = readline("Введи цифру: ");
+$type_bot = readline("Введи цифру: ");//test_new
 ?>
