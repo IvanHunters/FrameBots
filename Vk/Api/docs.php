337c193b8f3f20d091bd7bb02fc75e7c9a473295
IvanHunters <ohotnikovivan@yandex.ru>
2020-01-28 21:39:22 +0400

    edit ext file when was been download

--- Vk/Api/docs.php
+++ Vk/Api/docs.php
@@ -2,11 +2,34 @@
 namespace VK;
 
 trait Docs{
-    public function upload_file($file){
-        $upload_url = $this->apiCallGroup("docs.getMessagesUploadServer",array('type'=>'doc'))['response']['upload_url'];
+    
+    public function upload_file($file, $group = true){
+        
+        if(preg_match("/^doc/imu", $file)){
+            
+            $this->files_upload[] = $file;
+            return "none";
+            
+        }
+        
+        if(preg_match("/http|www/", $file)){
+            
+            preg_match("/[^.]+$/", $file, $ext);
+            $time_unix = time();
+            $new_file = "{$time_unix}.{$ext[0]}";
+            file_put_contents($new_file, file_get_contents($file));
+            $file = $new_file;
+            
+        }
+        
+        if($group) $method = "apiCallGroup";
+        else       $method = "apiCallUser";
+        
+        $upload_url = $this->{$method}("docs.getMessagesUploadServer",array('type'=>'doc', 'peer_id'=>$this->user_id))['response']['upload_url'];
         $aPost = array(
-            'file' => new CURLFile($file)
+            'file' => new \CURLFile($file)
         );
+        
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $upload_url);
         curl_setopt($ch, CURLOPT_SAFE_UPLOAD, true);
@@ -14,8 +37,11 @@ trait Docs{
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $res = json_decode(curl_exec($ch), true);
         curl_close($ch);
-        $get_data = $this->apiCallGroup("docs.save",array('file'=>$res['file']))['response'][0];
+        $attachment = $this->{$method}("docs.save",array('file'=>$res['file']))['response'];
+        $attach = "doc".$attachment[$attachment['type']]['owner_id']."_".$attachment[$attachment['type']]['id'];
         
-        return "doc".$get_data['owner_id']."_".$get_data['id'];
+        $this->files_upload[] = $attach;
+        if(file_exists($file))  unlink($file);
+            return $attach;
     }
 }
