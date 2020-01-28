08086842e0cfd09c66a6142c0a468b830d7b9292
IvanHunters <ohotnikovivan@yandex.ru>
2020-01-28 21:39:44 +0400

    edit ext file when was been download

--- Vk/Api/photos.php
+++ Vk/Api/photos.php
@@ -1,24 +1,31 @@
 <?php
 namespace VK;
+
 trait Photos{
     
     public function upload_photo($file, $group = true){
-        if(is_null($file)) fwrite(fopen("log", "a+"), "\nfile is null");
+        
         if(preg_match("/^photo/imu", $file)){
-            $this->files_upload = $file;
+            
+            $this->files_upload[] = $file;
             return "none";
+            
         }
         
-        if(preg_match("/http|www/", $file)){ 
+        if(preg_match("/http|www/", $file)){
+            preg_match("/[^.]+$/", $file, $ext);
             $time_unix = time();
-            $new_file = "{$time_unix}.jpg";
+            $new_file = "{$time_unix}.{$ext[0]}";
             file_put_contents($new_file, file_get_contents($file));
             $file = $new_file;
         }
+        
         if($group) $method = "apiCallGroup";
         else       $method = "apiCallUser";
         
         $upload_url = $this->{$method}("photos.getMessagesUploadServer", array())['response']['upload_url'];
+        
+        
         $aPost = array(
             'file' => new \CURLFile($file)
         );
@@ -29,9 +36,9 @@ trait Photos{
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $res = json_decode(curl_exec($ch), true);
         curl_close($ch);
-        $attachment = $this->{$method}("photos.saveMessagesPhoto", array("photo"=>$res['photo'], "server"=>$res['server'], "hash"=>$res['hash']))['response'];
-        $attach = "photo".$attachment[0]['owner_id']."_".$attachment[0]['id'];
-        $this->files_upload = $attach;
+        $attachment = $this->{$method}("photos.saveMessagesPhoto", array("photo"=>$res['photo'], "server"=>$res['server'], "hash"=>$res['hash']))['response'][0];
+        $attach = "photo".$attachment['owner_id']."_".$attachment['id'];
+        $this->files_upload[] = $attach;
         if(file_exists($file))  unlink($file);
             return $attach;
     
